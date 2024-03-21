2b4adc4757c472832af921beb69629135123d646
Krishu7827 <krishukumar7827@gmail.com>
2024-03-21 11:32:02 +0530

    json

--- Controller/IQCSolarCell.js
+++ Controller/IQCSolarCell.js
@@ -5,14 +5,63 @@
 
 
 /**to Add Solar Cell In IQC */
-const AddIQCSolarCell = (req,res)=>{
-  console.log(req.body)
+const AddIQCSolarCell = (req, res) => {
+  console.log(req.body.Rejected['CheckTypes'][0])
 }
 
 
 
+      /** Export Controllers */
+      module.exports = { AddIQCSolarCell }
 
 
 
-/** Export Controllers */
-module.exports = {AddIQCSolarCell}
+      // var SolarCell = {
+      //   'Packaging': {
+      //     'Characterstics': "packagingCharactersticsController.text",
+      //     'MeasuringMethod': "packagingMeasuringMethodController.text",
+      //     'Sampling': "packagingSamplingController.text",
+      //     'SmapleSize': "packagingSampleSizeController.text",
+      //     'Reference': "packagingReferenceDocController.text",
+      //     'AcceptanceCriteria': "packagingAcceptanceCriteriaController.text"
+          
+      //   },
+      //   'Visual': {
+      //     'Characterstics': "visualCharactersticsController.text",
+      //     'MeasuringMethod': "visualMeasuringMethodController.text",
+      //     'Sampling': "visualSamplingController.text",
+      //     'Reference': "visualReferenceDocController.text",
+      //     'AcceptanceCriteria': "visualAcceptanceCriteriaController.text"
+      //   },
+      //   'Physical': {
+      //     'Characterstics': "physicalCharactersticsController.text",
+      //     'MeasuringMethod': "physicalMeasuringMethodController.text",
+      //     'Sampling': "physicalSamplingController.text",
+      //     'Reference': "physicalReferenceDocController.text",
+      //     'AcceptanceCriteria': "physicalAcceptanceCriteriaController.text"
+          
+      //   },
+      //   'FrontBus': {
+      //     'Characterstics': "frontbusCharactersticsController.text",
+      //     'MeasuringMethod': "frontbusMeasuringMethodController.text",
+      //     'Sampling': "frontbusSamplingController.text",
+      //     'Reference': "frontbusReferenceDocController.text",
+      //     'AcceptanceCriteria': "frontbusAcceptanceCriteriaController.text"
+      //   },
+      //   'Verification': {
+      //     'Characterstics': "verificationCharactersticsController.text",
+      //     'MeasuringMethod': "verificationMeasuringMethodController.text",
+      //     'Sampling': "verificationSamplingController.text",
+      //     'Reference': "verificationReferenceDocController.text",
+      //     'AcceptanceCriteria': "verificationAcceptanceCriteriaController.text"
+      //   }
+      // };
+  
+      // var Rejected = {
+      //   "CheckTypes": [
+      //     {"Packaging": "packagingRejection"},
+      //     {"Visual": "visualRejection"},
+          
+      //   ],
+      //   "Reason": "rejectionReasonController"
+      // };
