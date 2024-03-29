17eb054c55e91e41a98c420540bb176284ffd666
Krishu7827 <krishukumar7827@gmail.com>
2024-03-29 15:05:40 +0530

    Upload Two Pdf

--- Controller/IQCSolarCell.js
+++ Controller/IQCSolarCell.js
@@ -258,7 +258,7 @@ const GetSpecificSolarCellTest = async (req, res) => {
   const { SolarDetailID } = req.body
 
   try {
-    const query = `SELECT id.SolarDetailID,id.LotSize,id.SupplierName,id.InvoiceNo,id.InvoiceDate,id.SupplierRMBatchNo,id.RawMaterialSpecs,id.QualityCheckDate,id.ReceiptDate,id.Status,i.IQCSolarID,i.CheckType,i.Samples,r.RejectedID,r.CheckTypes,r.Reason,r.Result FROM IQCSolarDetails id
+    const query = `SELECT id.SolarDetailID,id.LotSize,id.SupplierName,id.InvoiceNo,id.InvoiceDate,id.SupplierRMBatchNo,id.RawMaterialSpecs,id.QualityCheckDate,id.ReceiptDate,id.Status,i.IQCSolarID,i.CheckType,i.Samples,r.RejectedID,r.CheckTypes,r.Reason,r.Result,id.COCPdf,id.InvoicePdf FROM IQCSolarDetails id
     JOIN IQCSolar i ON id.SolarDetailID = i.SolarDetailID
     JOIN Rejected r ON id.SolarDetailID = r.SolarDetailID
     WHERE id.SolarDetailID = '${SolarDetailID}';`
