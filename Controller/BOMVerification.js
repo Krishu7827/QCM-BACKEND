93433daea5638783344a7df118bf431d6df5c2b8
Krishu7827 <krishukumar7827@gmail.com>
2024-04-08 15:14:38 +0530

    Employees List

--- Controller/BOMVerification.js
+++ Controller/BOMVerification.js
@@ -133,7 +133,7 @@ const AddBomVerification = async (req, res) => {
     try {
         /** Insert Bom Data in BomVerficationDetail Table */
         const BomVerificationDetailsQuery = `INSERT INTO BOMVerificationDetails(BOMDetailId,Type,RevNo,Date,Shift,Line,PONo,Status,CheckedBy,CreatedBy,CreatedOn)
-    VALUES ('${UUID}','BOM Verfication','${BomVerificationDetails['RevNo']}','${BomVerificationDetails['Date']}','${BomVerificationDetails['Shift']}','${BomVerificationDetails['Line']}','${BomVerificationDetails['PONo']}','${BomVerificationDetails['Status']}','${BomVerificationDetails['CurrentUser']}','${BomVerificationDetails['CurrentUser']}','${getCurrentDateTime()}');`
+    VALUES ('${UUID}','BOM Verification','${BomVerificationDetails['RevNo']}','${BomVerificationDetails['Date']}','${BomVerificationDetails['Shift']}','${BomVerificationDetails['Line']}','${BomVerificationDetails['PONo']}','${BomVerificationDetails['Status']}','${BomVerificationDetails['CurrentUser']}','${BomVerificationDetails['CurrentUser']}','${getCurrentDateTime()}');`
 
         await queryAsync(BomVerificationDetailsQuery)
 
