8e4b27ec2b4a97153ab0d4303ffa7629b819ec48
Krishu7827 <krishukumar7827@gmail.com>
2024-04-11 18:12:37 +0530

    Package-json update

--- Controller/PreLamController.js
+++ Controller/PreLamController.js
@@ -74,8 +74,8 @@ const AddPreLam = async(req,res)=>{
   const PreLam = req.body[1];
   const UUID = v4();
  try{
-  const PreLamDetailQuery = `INSERT INTO PreLamDetail(PreLamDetailId,DocNo,RevNo,Date,Shift,Line,PONo,CheckedBy,CreatedBy,CreatedOn)
-                                          VALUES('${UUID}','${PreLamDetail['DocNo']}','${PreLamDetail['RevNo']}','${PreLamDetail['Date']}','${PreLamDetail['Shift']}','${PreLamDetail['Line']}','${PreLamDetail['PONo']}','${PreLamDetail['CurrentUser']}','${PreLamDetail['CurrentUser']}','${getCurrentDateTime()}')`
+  const PreLamDetailQuery = `INSERT INTO PreLamDetail(PreLamDetailId,Type,DocNo,RevNo,Date,Shift,Line,PONo,CheckedBy,CreatedBy,CreatedOn,Status)
+                                          VALUES('${UUID}','PreLam','${PreLamDetail['DocNo']}','${PreLamDetail['RevNo']}','${PreLamDetail['Date']}','${PreLamDetail['Shift']}','${PreLamDetail['Line']}','${PreLamDetail['PONo']}','${PreLamDetail['CurrentUser']}','${PreLamDetail['CurrentUser']}','${getCurrentDateTime()}','${PreLamDetail['Status']}')`
 
         await queryAsync(PreLamDetailQuery);
     
@@ -93,7 +93,41 @@ const AddPreLam = async(req,res)=>{
 }
 
 
+const UploadPdf = async (req, res) => {
 
+  const { JobCardDetailId } = req.body;
+  /** Uploading PDF in S3 Bucket */
+  try {
+    const ReferencePdf = await new Promise((resolve, reject) => {
+      s3.upload({
+        Bucket: process.env.AWS_BUCKET_2,
+        Key: `${JobCardDetailId}_${req.file.originalname}`,
+        Body: req.file.buffer,
+        ACL: "public-read-write",
+        ContentType: req.file.mimetype
+      }, (err, result) => {
+        if (err) {
+          reject(err)
+        } else {
+
+          resolve(result)
+        }
+      })
+    });
+
+
+
+    const query = `UPDATE JobCardDetails jcd
+    set jcd.ReferencePdf = '${ReferencePdf.Location}'
+   WHERE jcd.JobCardDetailID = '${JobCardDetailId}';`;
+
+    const update = await queryAsync(query);
+    res.send({ msg: 'Data Inserted SuccesFully !', URL: ReferencePdf.Location });
+  } catch (err) {
+    console.log(err);
+    res.status(401).send(err);
+  }
+}
 
 
 
