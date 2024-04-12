6ed6dd67fa0d26cc0cd6605c7cf28515e673179a
Krishu7827 <krishukumar7827@gmail.com>
2024-04-12 11:46:28 +0530

    Package-json update

--- Controller/PreLamController.js
+++ Controller/PreLamController.js
@@ -8,88 +8,131 @@ const { dbConn } = require('../db.config/db.config');
 const queryAsync = util.promisify(dbConn.query).bind(dbConn);
 
 // let data = [{
-//     "CurrentUser":"5a114928-e8f3-11ee-b439-0ac93defbbf1",
-//     "DocNo":"GSPL/IPQC/IPC/003",
-//     "RevNo":"1.0 dated 12.08.2023",
-//     "Date":"DayController.text",
-//     "Shift":"shiftController.text",
-//     "Line":"lineController.text",
-//     "PONo":"PoController.text"
-//   },[
-//     {
-//       "Stage":"Glass Loader",
-//       "CheckPoint":{
-//         "Glass dimension(LengthxWidthxThickness)":"GlassLoaderGlassDimensionController.text",
-//         "Avaibility of WI":"GlassLoaderAvaibilityController.text",
-//       },
-//       "AcceptanceCriteria":{
-//         "Glass dimension(LengthxWidthxThickness)":"GlassLoaderCriteria1Controller.text",
-//         "Avaibility of WI":"GlassLoaderCriteria2Controller.text"
-//       },
-//       "Frequency":{
-//         "Glass dimension(LengthxWidthxThickness)":"GlassLoaderFreqquency1Controller.text",
-//         "Avaibility of WI":"GlassLoaderFrequency2Controller.text"
-//       },
-//       "Remark":"GlassLoaderRemarkController.text"
+//   "PreLamDetaildId": "",
+//   "CurrentUser": "5a114928-e8f3-11ee-b439-0ac93defbbf1",
+//   "DocNo": "GSPL/IPQC/IPC/003",
+//   "RevNo": "1.0 dated 12.08.2023",
+//   "Date": "DayController.text",
+//   "Shift": "shiftController.text",
+//   "Line": "lineController.text",
+//   "PONo": "PoController.text"
+// }, [
+//   {
+//     "Stage": "Glass Loader",
+//     "CheckPoint": {
+//       "Glass dimension(LengthxWidthxThickness)": "GlassLoaderGlassDimensionController.text",
+//       "Avaibility of WI": "GlassLoaderAvaibilityController.text",
 //     },
-//    {
-//       "Stage":"Glass side EVA cutting machine",
-//       "CheckPoint":{
-//         "EVA dimension{LengthxWidthxThickness}":"GlassEVADimensionController.text",
-//         "Cutting edge of EVA": "GlasCuttingEdgeController.text",
-//         "Position of front EVA":""
-//       },
-//       "AcceptanceCriteria":{
-//         "Glass dimension(LengthxWidthxThickness)":"GlassLoaderCriteria1Controller.text",
-//         "Avaibility of WI":"GlassLoaderCriteria2Controller.text"
-//       },
-//       "Frequency":{
-//         "Glass dimension(LengthxWidthxThickness)":"GlassLoaderFreqquency1Controller.text",
-//         "Avaibility of WI":"GlassLoaderFrequency2Controller.text"
-//       },
-//       "Remark":"GlassLoaderRemarkController.text"
+//     "AcceptanceCriteria": {
+//       "Glass dimension(LengthxWidthxThickness)": "GlassLoaderCriteria1Controller.text",
+//       "Avaibility of WI": "GlassLoaderCriteria2Controller.text"
 //     },
-//  {
-//       "Stage":"Cell Loading",
-//       "CheckPoint":{
-//         "Glass dimension(LengthxWidthxThickness)":"GlassLoaderGlassDimensionController.text",
-//         "string length & cell to cell Gap":["kdfksd","ksdfksdfk"]
-//       },
-//       "AcceptanceCriteria":{
-//         "Glass dimension(LengthxWidthxThickness)":"GlassLoaderCriteria1Controller.text",
-//         "Avaibility of WI":"GlassLoaderCriteria2Controller.text"
-//       },
-//       "Frequency":{
-//         "Glass dimension(LengthxWidthxThickness)": "GlassLoaderFreqquency1Controller.text",
-//         "string length & cell to cell Gap":"5 String"
-//       },
-//       "Remark": "GlassLoaderRemarkController.text"
-//     }
-    
-//   ]];
+//     "Frequency": {
+//       "Glass dimension(LengthxWidthxThickness)": "GlassLoaderFreqquency1Controller.text",
+//       "Avaibility of WI": "GlassLoaderFrequency2Controller.text"
+//     },
+//     "Remark": "GlassLoaderRemarkController.text"
+//   },
+//   {
+//     "Stage": "Glass side EVA cutting machine",
+//     "CheckPoint": {
+//       "EVA dimension{LengthxWidthxThickness}": "GlassEVADimensionController.text",
+//       "Cutting edge of EVA": "GlasCuttingEdgeController.text",
+//       "Position of front EVA": ""
+//     },
+//     "AcceptanceCriteria": {
+//       "Glass dimension(LengthxWidthxThickness)": "GlassLoaderCriteria1Controller.text",
+//       "Avaibility of WI": "GlassLoaderCriteria2Controller.text"
+//     },
+//     "Frequency": {
+//       "Glass dimension(LengthxWidthxThickness)": "GlassLoaderFreqquency1Controller.text",
+//       "Avaibility of WI": "GlassLoaderFrequency2Controller.text"
+//     },
+//     "Remark": "GlassLoaderRemarkController.text"
+//   },
+//   {
+//     "Stage": "Cell Loading",
+//     "CheckPoint": {
+//       "Glass dimension(LengthxWidthxThickness)": "GlassLoaderGlassDimensionController.text",
+//       "string length & cell to cell Gap": ["kdfksd", "ksdfksdfk"]
+//     },
+//     "AcceptanceCriteria": {
+//       "Glass dimension(LengthxWidthxThickness)": "GlassLoaderCriteria1Controller.text",
+//       "Avaibility of WI": "GlassLoaderCriteria2Controller.text"
+//     },
+//     "Frequency": {
+//       "Glass dimension(LengthxWidthxThickness)": "GlassLoaderFreqquency1Controller.text",
+//       "string length & cell to cell Gap": "5 String"
+//     },
+//     "Remark": "GlassLoaderRemarkController.text"
+//   }
 
-const AddPreLam = async(req,res)=>{
+// ]];
+
+const AddPreLam = async (req, res) => {
   console.log(req.body)
   const PreLamDetail = req.body[0];
   const PreLam = req.body[1];
+  const PreLamDetaildId = PreLamDetail['PreLamDetaildId']
   const UUID = v4();
- try{
-  const PreLamDetailQuery = `INSERT INTO PreLamDetail(PreLamDetailId,Type,DocNo,RevNo,Date,Shift,Line,PONo,CheckedBy,CreatedBy,CreatedOn,Status)
+  if (!PreLamDetaildId) {
+    try {
+      const PreLamDetailQuery = `INSERT INTO PreLamDetail(PreLamDetailId,Type,DocNo,RevNo,Date,Shift,Line,PONo,CheckedBy,CreatedBy,CreatedOn,Status)
                                           VALUES('${UUID}','PreLam','${PreLamDetail['DocNo']}','${PreLamDetail['RevNo']}','${PreLamDetail['Date']}','${PreLamDetail['Shift']}','${PreLamDetail['Line']}','${PreLamDetail['PONo']}','${PreLamDetail['CurrentUser']}','${PreLamDetail['CurrentUser']}','${getCurrentDateTime()}','${PreLamDetail['Status']}')`
 
-        await queryAsync(PreLamDetailQuery);
-    
-        PreLam.forEach(async(Lam)=>{
-          const PreLamQuery = `INSERT INTO PreLam(PreLamId,PreLamDetailId,Stage,CheckPoint,Frequency,AcceptanceCriteria,Remark)
-                                          VALUES('${v4()}','${UUID}','${Lam['Stage']}','${JSON.stringify(Lam['CheckPoint'])}','${JSON.stringify(Lam['Frequency'])}','${JSON.stringify(Lam['AcceptanceCriteria'])}','${Lam['Remark']}')`
-          await queryAsync(PreLamQuery);
+      await queryAsync(PreLamDetailQuery);
 
-        })
-        res.send({ msg: 'Data Inserted Succesfully !', UUID });
- }catch(err){
-   console.log(err)
-   res.status(400).send(err);
- }
+      PreLam.forEach(async (Lam) => {
+        const PreLamQuery = `INSERT INTO PreLam(PreLamId,PreLamDetailId,Stage,CheckPoint,Frequency,AcceptanceCriteria,Remark)
+                                          VALUES('${v4()}','${UUID}','${Lam['Stage']}','${JSON.stringify(Lam['CheckPoint'])}','${JSON.stringify(Lam['Frequency'])}','${JSON.stringify(Lam['AcceptanceCriteria'])}','${Lam['Remark']}');`
+        await queryAsync(PreLamQuery);
+
+      })
+      res.send({ msg: 'Data Inserted Succesfully !', UUID });
+    } catch (err) {
+      console.log(err)
+      res.status(400).send(err);
+    }
+  } else {
+
+    try {
+      const PreLamDetailQuery = `UPDATE PreLamDetail
+   SET
+     DocNo = '${PreLamDetail['DocNo']}',
+     RevNo = '${PreLamDetail['RevNo']}',
+     Date = '${PreLamDetail['Date']}',
+     Shift = '${PreLamDetail['Shift']}',
+     Line = '${PreLamDetail['Line']}',
+     PONo = '${PreLamDetail['PONo']}',
+     CheckedBy = '${PreLamDetail['CurrentUser']}',
+     CreatedBy = '${PreLamDetail['CurrentUser']}',
+     Status = '${PreLamDetail['Status']}'
+   WHERE PreLamDetailId = '${PreLamDetail['PreLamDetaildId']}';
+     `
+      await queryAsync(PreLamDetailQuery);
+
+      PreLam.forEach(async (Lam) => {
+        // const PreLamQuery = `INSERT INTO PreLam(PreLamId,PreLamDetailId,Stage,CheckPoint,Frequency,AcceptanceCriteria,Remark)
+        // VALUES('${v4()}','${UUID}','${Lam['Stage']}','${JSON.stringify(Lam['CheckPoint'])}','${JSON.stringify(Lam['Frequency'])}','${JSON.stringify(Lam['AcceptanceCriteria'])}','${Lam['Remark']}');`
+
+        const PreLamQuery = `UPDATE PreLam
+     SET
+       CheckPoint = '${JSON.stringify(Lam['CheckPoint'])}',
+       Frequency = '${JSON.stringify(Lam['Frequency'])}',
+       AcceptanceCriteria = '${JSON.stringify(Lam['AcceptanceCriteria'])}',
+       Remark = '${Lam['Remark']}'
+     WHERE PreLamDetailId = '${PreLamDetail['PreLamDetaildId']}' AND Stage = '${Lam['Stage']}';
+       `
+        await queryAsync(PreLamQuery);
+      })
+      res.send({ msg: 'Data Inserted Succesfully !',UUID:PreLamDetaildId });
+    } catch (err) {
+      
+      console.log(err)
+      res.status(400).send({err})
+    }
+  }
+
 }
 
 
@@ -134,4 +177,4 @@ const UploadPdf = async (req, res) => {
 
 
 
-module.exports = {AddPreLam}
+module.exports = { AddPreLam }
