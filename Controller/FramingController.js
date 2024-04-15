119c5bf5e37837e4ede77e9522532f2d04ac7745
Krishu7827 <krishukumar7827@gmail.com>
2024-04-15 18:33:47 +0530

    Add Framing Detail Endpoint

--- Controller/FramingController.js
+++ Controller/FramingController.js
@@ -7,92 +7,92 @@ const { dbConn } = require('../db.config/db.config');
 /** Making Sync To Query */
 const queryAsync = util.promisify(dbConn.query).bind(dbConn);
 
-const data = {
-     'PreLamDetailId':'',
-    'CurrentUser':'',
-    'Status':'Inprogress',
-    'DocNo':'GSPL/IPQC/AF/011',
-    'RevNo':'1.0/12.08.2023',
-    'Date': dateController.text,
-    'Shift': shiftController.text,
-    'samples': [
-      {
-        'Sample': Sample1Controller.text,
-        'FramingObservation': Sample1GlueController.text,
-        'FramingDimension': {
-          'x1': Sample1x1Controller.text,
-          'x2': Sample1x2Controller.text,
-          'y1': Sample1y1Controller.text,
-          'y2': Sample1y2Controller.text,
-          'l1': Sample1L1Controller.text,
-          'l2': Sample1L2Controller.text,
-          'w1': Sample1W1Controller.text,
-          'w2': Sample1W2Controller.text
-        }
-      },
-      {
-        'Sample': Sample2Controller.text,
-        'FramingObservation': Sample2GlueController.text,
-        'FramingDimension': {
-          'x1': Sample2x1Controller.text,
-          'x2': Sample2x2Controller.text,
-          'y1': Sample2y1Controller.text,
-          'y2': Sample2y2Controller.text,
-          'l1': Sample2L1Controller.text,
-          'l2': Sample2L2Controller.text,
-          'w1': Sample2W1Controller.text,
-          'w2': Sample2W2Controller.text
-        }
-      },
-      {
-        'Sample': Sample3Controller.text,
-        'FramingObservation': Sample3GlueController.text,
-        'FramingDimension': {
-          'x1': Sample3x1Controller.text,
-          'x2': Sample3x2Controller.text,
-          'y1': Sample3y1Controller.text,
-          'y2': Sample3y2Controller.text,
-          'l1': Sample3L1Controller.text,
-          'l2': Sample3L2Controller.text,
-          'w1': Sample3W1Controller.text,
-          'w2': Sample3W2Controller.text
-        }
-      },
-      {
-        'Sample': Sample4Controller.text,
-        'FramingObservation': Sample4GlueController.text,
-        'FramingDimension': {
-          'x1': Sample4x1Controller.text,
-          'x2': Sample4x2Controller.text,
-          'y1': Sample4y1Controller.text,
-          'y2': Sample4y2Controller.text,
-          'l1': Sample4L1Controller.text,
-          'l2': Sample4L2Controller.text,
-          'w1': Sample4W1Controller.text,
-          'w2': Sample4W2Controller.text
-        }
-      },
-      {
-        'Sample': Sample5Controller.text,
-        'FramingObservation': Sample5GlueController.text,
-        'FramingDimension': {
-          'x1': Sample5x1Controller.text,
-          'x2': Sample5x2Controller.text,
-          'y1': Sample5y1Controller.text,
-          'y2': Sample5y2Controller.text,
-          'l1': Sample5L1Controller.text,
-          'l2': Sample5L2Controller.text,
-          'w1': Sample5W1Controller.text,
-          'w2': Sample5W2Controller.text
-        }
-      },
-    ],
-  }
+// const data = {
+//      'PreLamDetailId':'',
+//     'CurrentUser':'',
+//     'Status':'Inprogress',
+//     'DocNo':'GSPL/IPQC/AF/011',
+//     'RevNo':'1.0/12.08.2023',
+//     'Date': dateController.text,
+//     'Shift': shiftController.text,
+//     'samples': [
+//       {
+//         'Sample': Sample1Controller.text,
+//         'FramingObservation': Sample1GlueController.text,
+//         'FramingDimension': {
+//           'x1': Sample1x1Controller.text,
+//           'x2': Sample1x2Controller.text,
+//           'y1': Sample1y1Controller.text,
+//           'y2': Sample1y2Controller.text,
+//           'l1': Sample1L1Controller.text,
+//           'l2': Sample1L2Controller.text,
+//           'w1': Sample1W1Controller.text,
+//           'w2': Sample1W2Controller.text
+//         }
+//       },
+//       {
+//         'Sample': Sample2Controller.text,
+//         'FramingObservation': Sample2GlueController.text,
+//         'FramingDimension': {
+//           'x1': Sample2x1Controller.text,
+//           'x2': Sample2x2Controller.text,
+//           'y1': Sample2y1Controller.text,
+//           'y2': Sample2y2Controller.text,
+//           'l1': Sample2L1Controller.text,
+//           'l2': Sample2L2Controller.text,
+//           'w1': Sample2W1Controller.text,
+//           'w2': Sample2W2Controller.text
+//         }
+//       },
+//       {
+//         'Sample': Sample3Controller.text,
+//         'FramingObservation': Sample3GlueController.text,
+//         'FramingDimension': {
+//           'x1': Sample3x1Controller.text,
+//           'x2': Sample3x2Controller.text,
+//           'y1': Sample3y1Controller.text,
+//           'y2': Sample3y2Controller.text,
+//           'l1': Sample3L1Controller.text,
+//           'l2': Sample3L2Controller.text,
+//           'w1': Sample3W1Controller.text,
+//           'w2': Sample3W2Controller.text
+//         }
+//       },
+//       {
+//         'Sample': Sample4Controller.text,
+//         'FramingObservation': Sample4GlueController.text,
+//         'FramingDimension': {
+//           'x1': Sample4x1Controller.text,
+//           'x2': Sample4x2Controller.text,
+//           'y1': Sample4y1Controller.text,
+//           'y2': Sample4y2Controller.text,
+//           'l1': Sample4L1Controller.text,
+//           'l2': Sample4L2Controller.text,
+//           'w1': Sample4W1Controller.text,
+//           'w2': Sample4W2Controller.text
+//         }
+//       },
+//       {
+//         'Sample': Sample5Controller.text,
+//         'FramingObservation': Sample5GlueController.text,
+//         'FramingDimension': {
+//           'x1': Sample5x1Controller.text,
+//           'x2': Sample5x2Controller.text,
+//           'y1': Sample5y1Controller.text,
+//           'y2': Sample5y2Controller.text,
+//           'l1': Sample5L1Controller.text,
+//           'l2': Sample5L2Controller.text,
+//           'w1': Sample5W1Controller.text,
+//           'w2': Sample5W2Controller.text
+//         }
+//       },
+//     ],
+//   }
 
 
 const AddFraming = async(req,res)=>{
-  const {DocNo,RevNo,Shift,Date,CurrentUser,Status,PreLamDetailId} = data;
-  const Samples = data['samples'];
+  const {DocNo,RevNo,Shift,Date,CurrentUser,Status,PreLamDetailId} = req.body;
+  const Samples = req.body['samples'];
   const UUID = v4();
   if(!PreLamDetailId){
   try{
