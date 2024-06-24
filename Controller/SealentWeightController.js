d4d9fe2424cd67a16ddba2e1fd83327afd436e55
Krishu7827 <krishukumar7827@gmail.com>
2024-06-24 16:23:08 +0530

    PDF

--- Controller/SealentWeightController.js
+++ Controller/SealentWeightController.js
@@ -2,6 +2,7 @@ const { v4: uuidv4, v4 } = require('uuid');
 const { getCurrentDateTime, s3 } = require('../Utilis/BOMVerificationUtilis')
 const util = require('util');
 const fs = require('fs');
+const {SealentExcel} = require('../Utilis/BOMVerificationUtilis')
 const Path = require('path');
 const { dbConn } = require('../db.config/db.config')
 require('dotenv').config()
@@ -213,7 +214,34 @@ const UpdateSealentStatus = async(req,res)=>{
   
       let UpdateStatus =  await queryAsync(UpdateStatusQuery);
   
-      res.send({status:true,data:UpdateStatus});
+      let PreLamQuery = `  select *FROM SealentWeight PL
+    JOIN PreLamDetail PD ON PD.PreLamDetailId = PL.PreLamDetailId
+    JOIN Person P on PD.CreatedBy = P.PersonID
+    WHERE PD.PreLamDetailId = '${JobCardDetailId}';`
+
+   let PreLamData = await queryAsync(PreLamQuery);
+    let Name = await  queryAsync(`SELECT Name FROM Person WHERE PersonID = '${CurrentUser}';`);
+
+  PreLamData.length?Name.length?PreLamData[0]['ReviewedBy'] = Name[0]['Name']:PreLamData[0]['ReviewedBy'] = 'Unknown':''
+  ExcelFileName = await SealentExcel(PreLamData);
+
+  try{
+    //  let ExcelFileName = await PreLamExcel(PreLamData);
+      
+      let URL = `http://srv515471.hstgr.cloud:${PORT}/IQCSolarCell/Excel/${ExcelFileName}`
+      let ExcelQuery = `UPDATE PreLamDetail JD
+      set JD.ExcelURL = '${URL}'
+      WHERE PreLamDetailId = '${JobCardDetailId}';`
+  
+      await queryAsync(ExcelQuery);
+    return  res.send({URL:`http://srv515471.hstgr.cloud:${PORT}/IQCSolarCell/Excel/${ExcelFileName}`});
+  
+       }catch(err){
+        console.log(err)
+       return  res.status(400).send(err)
+  
+       }
+       
     }catch(err){
       console.log(err)
       res.status(400).send({status:false,err})
