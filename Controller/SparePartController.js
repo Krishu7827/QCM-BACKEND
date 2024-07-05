438d5878a441523d7229f9f65a74c28490ddbd6c
Krishu7827 <krishukumar7827@gmail.com>
2024-07-05 15:00:30 +0530

    Mulitple Image function

--- Controller/SparePartController.js
+++ Controller/SparePartController.js
@@ -1,10 +1,10 @@
 const { v4: uuidv4, v4 } = require('uuid');
-const {getCurrentDateTime} = require('../Utilis/MaintenanceUtilis')
+const { getCurrentDateTime } = require('../Utilis/MaintenanceUtilis')
 const util = require('util');
 const fs = require('fs');
 const Path = require('path');
 const { dbConn } = require('../db.config/db.config');
-const { json } = require('express');
+const { json, query } = require('express');
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 
@@ -15,51 +15,54 @@ const queryAsync = util.promisify(dbConn.query).bind(dbConn);
 
 /**Add Machine Data  */
 const AddSpareParts = async (req, res) => {
-    const { 
-        SparePartName,
-        SpareNumber,
-        Specification,
-        BrandName,
-        MachineName,
-        Status,
-        MasterSparePartName,
-        CurrentUser: CreatedBy } = req.body;
+  const {
+    SparePartName,
+    SpareNumber,
+    Specification,
+    BrandName,
+    MachineName,
+    Status,
+    MasterSparePartName,
+    NumberOfPcs,
+    CycleTime,
+    Equivalent,
+    CurrentUser: CreatedBy } = req.body;
 
 
-    console.log(req.body)
-    console.log('typeeeeeeeeeeeeeeeee',typeof MachineName)
-    const MachineNameArray = MachineName
-    let UUID = v4();
-    try {
-      let getSpareModelNumberQuery = `SELECT SpareNumber FROM SparePartName WHERE SpareNumber = '${SpareNumber}';`
-      let getSpareModelNumbers = await queryAsync(getSpareModelNumberQuery);
+  console.log(req.body)
+  console.log('typeeeeeeeeeeeeeeeee', typeof MachineName)
+  const MachineNameArray = MachineName
+  let UUID = v4();
+  try {
+    let getSpareModelNumberQuery = `SELECT SpareNumber FROM SparePartName WHERE SpareNumber = '${SpareNumber}';`
+    let getSpareModelNumbers = await queryAsync(getSpareModelNumberQuery);
 
-      if(getSpareModelNumbers.length){
+    if (getSpareModelNumbers.length) {
 
-        return res.status(409).send({msg:'Duplicate Spare Model Number'})
-      }
+      return res.status(409).send({ msg: 'Duplicate Spare Model Number' })
+    }
 
-      const query = `INSERT INTO SparePartName(SparPartId ,SparePartName,SpareNumber,Specification,BrandName,Status,CreatedBy,MasterSparePartName,CreatedOn) VALUES
-            ('${UUID}','${SparePartName}','${SpareNumber}','${Specification}','${BrandName}','${Status}','${CreatedBy}','${MasterSparePartName}','${getCurrentDateTime()}');`
+    const query = `INSERT INTO SparePartName(SparPartId ,SparePartName,SpareNumber,Specification,BrandName,Status,CreatedBy,MasterSparePartName,CreatedOn,NumberOfPcs,CycleTime,Equivalent) VALUES
+            ('${UUID}','${SparePartName}','${SpareNumber}','${Specification}','${BrandName}','${Status}','${CreatedBy}','${MasterSparePartName}','${getCurrentDateTime()}','${NumberOfPcs}','${CycleTime}','${JSON.stringify(Equivalent)}');`
+
+    await queryAsync(query)
+    MachineNameArray.forEach(async (MachineName) => {
+      let SpareMachineId = v4();
+      let query = `INSERT INTO SparePartMachine(SparePartMachineId,SparePartId,MachineId,Status,CreatedBy,CreatedOn) VALUES
+                    ('${SpareMachineId}','${UUID}','${MachineName}','${Status}','${CreatedBy}','${getCurrentDateTime()}');`
 
       await queryAsync(query)
-        MachineNameArray.forEach(async(MachineName)=>{
-          let SpareMachineId = v4();
-         let query = `INSERT INTO SparePartMachine(SparePartMachineId,SparePartId,MachineId,Status,CreatedBy,CreatedOn) VALUES
-                    ('${SpareMachineId}','${UUID}','${MachineName}','${Status}','${CreatedBy}','${getCurrentDateTime()}');`
-        
-                    await queryAsync(query)
-        });
-          
- 
-      return  res.send({ msg: 'Inserted Succesfully!', SparePartId: UUID });
+    });
 
-    } catch (err) {
 
-        console.log(err)
-     return   res.status(400).send({ err })
+    return res.send({ msg: 'Inserted Succesfully!', SparePartId: UUID });
 
-    }
+  } catch (err) {
+
+    console.log(err)
+    return res.status(400).send({ err })
+
+  }
 
 
 
@@ -68,60 +71,60 @@ const AddSpareParts = async (req, res) => {
 
 
 const UploadImage = async (req, res) => {
-   console.log(req.files)
-    const { SparePartId } = req.body;
-    
-    /** Uploading PDF in Employee-Profile-Folder */
-      try {
-        /** Get the file buffer and the file format */
-        if(req.files['SparePartImage'] && req.files['DrawingImage']){
+  console.log(req.files)
+  const { SparePartId } = req.body;
 
-        const DrawingImageBuffer = req.files['DrawingImage'][0].buffer;
-         let DrawingImage =  req.files['DrawingImage'][0].originalname.split('.');
-         let DrawingFileFormat = DrawingImage[DrawingImage.length-1];
+  /** Uploading PDF in Employee-Profile-Folder */
+  try {
+    /** Get the file buffer and the file format */
+    if (req.files['SparePartImage'] && req.files['DrawingImage']) {
+
+      const DrawingImageBuffer = req.files['DrawingImage'][0].buffer;
+      let DrawingImage = req.files['DrawingImage'][0].originalname.split('.');
+      let DrawingFileFormat = DrawingImage[DrawingImage.length - 1];
 
 
-        /** Define the folder path */
-        const folderPath = Path.join('SpartPartImage');
-  
-        /** Create the folder if it doesn't exist */
-        if (!fs.existsSync(folderPath)) {
-          fs.mkdirSync(folderPath, { recursive: true });
-        }
-  
-        /** Define the file path, including the desired file name and format */
-        const COCFileName = `${SparePartId}_Drawing.${DrawingFileFormat}`;
+      /** Define the folder path */
+      const folderPath = Path.join('SpartPartImage');
 
-        const COCFilePath = Path.join(folderPath,COCFileName);
-  
-        /** Save the file buffer to the specified file path */
-        fs.writeFileSync(COCFilePath, DrawingImageBuffer);
-        
-        let SparePartImages = req.files['SparePartImage']
-          
-       let ImagesURL =  SparePartImages.map((image)=>{
-            const UUID = v4()
+      /** Create the folder if it doesn't exist */
+      if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, { recursive: true });
+      }
+
+      /** Define the file path, including the desired file name and format */
+      const COCFileName = `${SparePartId}_Drawing.${DrawingFileFormat}`;
+
+      const COCFilePath = Path.join(folderPath, COCFileName);
+
+      /** Save the file buffer to the specified file path */
+      fs.writeFileSync(COCFilePath, DrawingImageBuffer);
+
+      let SparePartImages = req.files['SparePartImage']
+
+      let ImagesURL = SparePartImages.map((image) => {
+        const UUID = v4()
         const SparePartImageBuffer = image.buffer;
         let SparePartImage = image.originalname.split('.');
-        let SparePartFileFormat = SparePartImage[SparePartImage.length-1];
+        let SparePartFileFormat = SparePartImage[SparePartImage.length - 1];
 
         /** Define the file path, including the desired file name and format */
         const InvoiceFileName = `${UUID}_SparePart.${SparePartFileFormat}`;
         const InvoceFilePath = Path.join(folderPath, InvoiceFileName);
 
-         /** Save the file buffer to the specified file path */
-         fs.writeFileSync(InvoceFilePath, SparePartImageBuffer);
+        /** Save the file buffer to the specified file path */
+        fs.writeFileSync(InvoceFilePath, SparePartImageBuffer);
 
-         return `http://srv515471.hstgr.cloud:${PORT}/Maintenance/File/${InvoiceFileName}`;
-        })
-        
-        console.log(ImagesURL)
-        const query = `UPDATE SparePartName id
+        return `http://srv515471.hstgr.cloud:${PORT}/Maintenance/File/${InvoiceFileName}`;
+      })
+
+      console.log(ImagesURL)
+      const query = `UPDATE SparePartName id
         set id.SparePartDrawingImageURL = 'http://srv515471.hstgr.cloud:${PORT}/Maintenance/File/${COCFileName}',
          id.SparePartImageURL = '${JSON.stringify(ImagesURL)}'
        WHERE id.SparPartId = '${SparePartId}';`;
 
-       let data = await new Promise((resolve, rejects) => {
+      let data = await new Promise((resolve, rejects) => {
         dbConn.query(query, (err, result) => {
           if (err) {
             rejects(err)
@@ -130,101 +133,126 @@ const UploadImage = async (req, res) => {
           }
         })
       })
-    return res.send({ msg: 'Data Inserted SuccesFully !' })
+      return res.send({ msg: 'Data Inserted SuccesFully !' })
 
-    }else if(req.files['SparePartImage']){
+    } else if (req.files['SparePartImage']) {
 
-       /** Define the folder path */
-       const folderPath = Path.join('SpartPartImage');
-  
-       /** Create the folder if it doesn't exist */
-       if (!fs.existsSync(folderPath)) {
-         fs.mkdirSync(folderPath, { recursive: true });
-       }
- 
-       let SparePartImages = req.files['SparePartImage']
-          
-       let ImagesURL =  SparePartImages.map((image)=>{
-            const UUID = v4()
+      /** Define the folder path */
+      const folderPath = Path.join('SpartPartImage');
+
+      /** Create the folder if it doesn't exist */
+      if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, { recursive: true });
+      }
+
+      let SparePartImages = req.files['SparePartImage']
+
+      let ImagesURL = SparePartImages.map((image) => {
+        const UUID = v4()
         const SparePartImageBuffer = image.buffer;
         let SparePartImage = image.originalname.split('.');
-        let SparePartFileFormat = SparePartImage[SparePartImage.length-1];
+        let SparePartFileFormat = SparePartImage[SparePartImage.length - 1];
 
         /** Define the file path, including the desired file name and format */
         const InvoiceFileName = `${UUID}_SparePart.${SparePartFileFormat}`;
         const InvoceFilePath = Path.join(folderPath, InvoiceFileName);
 
-         /** Save the file buffer to the specified file path */
-         fs.writeFileSync(InvoceFilePath, SparePartImageBuffer);
+        /** Save the file buffer to the specified file path */
+        fs.writeFileSync(InvoceFilePath, SparePartImageBuffer);
 
-         return `http://srv515471.hstgr.cloud:${PORT}/Maintenance/File/${InvoiceFileName}`;
-        })
-        
-        const query = `UPDATE SparePartName id
+        return `http://srv515471.hstgr.cloud:${PORT}/Maintenance/File/${InvoiceFileName}`;
+      })
+
+      const query = `UPDATE SparePartName id
         set id.SparePartImageURL = '${JSON.stringify(ImagesURL)}'
        WHERE id.SparPartId = '${SparePartId}';`;
 
-       await queryAsync(query);
-       return res.send({ msg: 'Data Inserted SuccesFully !' })
+      await queryAsync(query);
+      return res.send({ msg: 'Data Inserted SuccesFully !' })
 
-    }else if(req.files['DrawingImage']){
+    } else if (req.files['DrawingImage']) {
 
       const DrawingImageBuffer = req.files['DrawingImage'][0].buffer;
-      let DrawingImage =  req.files['DrawingImage'][0].originalname.split('.');
-      let DrawingFileFormat = DrawingImage[DrawingImage.length-1];
+      let DrawingImage = req.files['DrawingImage'][0].originalname.split('.');
+      let DrawingFileFormat = DrawingImage[DrawingImage.length - 1];
 
 
-     /** Define the folder path */
-     const folderPath = Path.join('SpartPartImage');
+      /** Define the folder path */
+      const folderPath = Path.join('SpartPartImage');
 
-     /** Create the folder if it doesn't exist */
-     if (!fs.existsSync(folderPath)) {
-       fs.mkdirSync(folderPath, { recursive: true });
-     }
+      /** Create the folder if it doesn't exist */
+      if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, { recursive: true });
+      }
 
-     /** Define the file path, including the desired file name and format */
-     const COCFileName = `${SparePartId}_Drawing.${DrawingFileFormat}`;
+      /** Define the file path, including the desired file name and format */
+      const COCFileName = `${SparePartId}_Drawing.${DrawingFileFormat}`;
 
-     const COCFilePath = Path.join(folderPath,COCFileName);
+      const COCFilePath = Path.join(folderPath, COCFileName);
 
-     /** Save the file buffer to the specified file path */
-     fs.writeFileSync(COCFilePath, DrawingImageBuffer);
-     
-     const query = `UPDATE SparePartName id
+      /** Save the file buffer to the specified file path */
+      fs.writeFileSync(COCFilePath, DrawingImageBuffer);
+
+      const query = `UPDATE SparePartName id
      set id.SparePartDrawingImageURL = 'http://srv515471.hstgr.cloud:${PORT}/Maintenance/File/${COCFileName}'
     WHERE id.SparPartId = '${SparePartId}';`;
 
-    await queryAsync(query);
-    return res.send({ msg: 'Data Inserted SuccesFully !' });
+      await queryAsync(query);
+      return res.send({ msg: 'Data Inserted SuccesFully !' });
 
-    }else{
+    } else {
 
       return res.send({ msg: 'Data Inserted SuccesFully !' });
     }
-  
-      } catch (err) {
-        console.log(err);
-        res.status(401).send(err);
+
+  } catch (err) {
+    console.log(err);
+    res.status(401).send(err);
+  }
+
+}
+
+
+const GetImage = async (req, res) => {
+  const filename = req.params.filename;
+  /** Define the absolute path to the IPQC-Pdf-Folder directory */
+  const pdfFolderPath = Path.resolve('SpartPartImage');
+
+  /** Construct the full file path to the requested file */
+  const filePath = Path.join(pdfFolderPath, filename);
+
+  /** Send the file to the client */
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      console.error('Error sending file:', err);
+      res.status(404).send({ error: 'File not found' });
+    }
+  });
+}
+
+
+const getEquivalent = async (req, res) => {
+  const { SparePartName, MachineName } = req.body;
+  query = `SELECT SP.SparePartName, SP.SpareNumber FROM SparePartName SP
+    JOIN SparePartMachine SPM ON SPM.SparePartId = SP.SparPartId
+    WHERE SP.SparePartName = '${SparePartName}' AND`
+  try {
+
+    MachineName.forEach((machine, i) => {
+
+      if (MachineName.length - 1 == i) {
+        query += ` SP.MachineId = '${machine}';`
+      } else {
+        query += ` SP.MachineId = '${machine}' OR`
       }
-   
+
+    })
+    let data = await queryAsync(query);
+    res.send(data);
+  } catch (err) {
+res.send(err)
   }
+}
 
 
-  const GetImage = async(req,res)=>{
-    const filename = req.params.filename;
-     /** Define the absolute path to the IPQC-Pdf-Folder directory */
-     const pdfFolderPath = Path.resolve('SpartPartImage');
-  
-     /** Construct the full file path to the requested file */
-     const filePath = Path.join(pdfFolderPath, filename);
-  
-     /** Send the file to the client */
-     res.sendFile(filePath, (err) => {
-         if (err) {
-             console.error('Error sending file:', err);
-             res.status(404).send({ error: 'File not found' });
-         }
-     });
-  }
-
-module.exports = {AddSpareParts, UploadImage, AddSpareParts, GetImage};
+module.exports = { AddSpareParts, UploadImage, AddSpareParts, GetImage };
